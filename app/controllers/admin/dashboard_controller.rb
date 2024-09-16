098f8c12cea36a7b8503342d03e89b7bf9b52bb9
Ahmad <36176055+ItsAhmad@users.noreply.github.com>
2024-09-16 12:41:58 -0400

    Implement HTTPS Authentication

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,11 @@
 class Admin::DashboardController < ApplicationController
-  def show
+  before_action :authenticate
+  
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_basic('Administration') do |username, password|
+      username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
+    end
   end
 end
