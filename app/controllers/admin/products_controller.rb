949ee4fa18ad126b1f385bcc84d197d1a28ccbd8
Ahmad <36176055+ItsAhmad@users.noreply.github.com>
2024-09-16 12:41:20 -0400

    Implement HTTPS Authentication

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -37,4 +37,10 @@ class Admin::ProductsController < ApplicationController
     )
   end
 
+def authenticate
+    authenticate_or_request_with_http_basic('Administration') do |username, password|
+      username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
+    end
+  end
+
 end
